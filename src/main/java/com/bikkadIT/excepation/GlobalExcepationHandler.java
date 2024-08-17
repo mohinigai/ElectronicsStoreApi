1197bc8b0a29271f6123c77e8591f40ab3ae3eb1
mohinigai <you@example.com>
2024-08-17 23:24:17 +0530

    V1:B-123456- Logger Applied on Globle exception Handler

--- src/main/java/com/bikkadIT/excepation/GlobalExcepationHandler.java
+++ src/main/java/com/bikkadIT/excepation/GlobalExcepationHandler.java
@@ -3,6 +3,8 @@ package com.bikkadIT.excepation;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,14 +14,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.bikkadIT.payload.ApiResponse;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExcepationHandler {
 
+	private static Logger log = LoggerFactory.getLogger(GlobalExcepationHandler.class);
+	
 	@ExceptionHandler(ResourceNotFoundExcption.class)
 	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundExcption ex) {
+		
+		log.info("Logger Exception handler!!");
 		String message = ex.getMessage();
+		
 		ApiResponse apiResponse = new ApiResponse(message, false);
 		return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
 }
