cd15e7ed010a816e87c14c48fd7f22baae304cc4
mohinigai <you@example.com>
2024-11-09 18:07:47 +0530

    Annotaions provided

--- src/main/java/com/bikkadIT/entity/Book.java
+++ src/main/java/com/bikkadIT/entity/Book.java
@@ -1,7 +1,17 @@
 package com.bikkadIT.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
 public class Book {
 
+    @javax.persistence.Id
     private String Id;
     private String name;
     private String Author;
